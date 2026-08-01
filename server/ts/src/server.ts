ae3db7db3c67390ffa5b857f4e140c228b0f59ce
Waiwaitiyyt <waiwaitiyyt@outlook.com>
2026-08-01 13:13:40 +0800

    initial commit

---
+++ server/ts/src/server.ts
@@ -0,0 +1,6 @@
+// This is the backend server under express architecture
+import express, {type Express, type Request, type Response} from "express"
+
+const app: Express = express();
+
+console.log("Bonjour");
---
+++ tools/src/fetch_dataset.py
@@ -0,0 +1,6 @@
+# This is a script to automatically download the dataset from kaggle
+import kagglehub
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
