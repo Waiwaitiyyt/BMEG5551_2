9ee0a67962d4f1252f5c82c3f42d142603b37466
Waiwaitiyyt <waiwaitiyyt@outlook.com>
2026-08-02 17:08:30 +0800

    Added datasets

--- tools/src/fetch_dataset.py
+++ tools/src/fetch_dataset.py
@@ -1,6 +1,14 @@
 # This is a script to automatically download the dataset from kaggle
-import kagglehub
-import matplotlib.pyplot as plt
+import os
 from pathlib import Path
 
+root_dir = Path(__file__).parent.parent.parent
+os.environ["KAGGLEHUB_CACHE"] = str(root_dir) + r"\datasets" # If in Linux, change the path to "/datasets" instead of "\datasets"
 
+import kagglehub
+import matplotlib.pyplot as plt
+
+
+path = kagglehub.dataset_download("tawsifurrahman/aseptic-loose-hip-implant-xray-database")
+
+print("Path to dataset files:", path)
