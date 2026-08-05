2a1823db0fae4211f73dacf76056df4a35980052
yangtian <23917353@student.uwa.edu.au>
2026-08-05 11:49:52 +0000

    added a setup script for VM

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# Setting up script for linux VM 
+
+# Install uv
+echo "Installing uv..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+echo "uv installed successfully."
+
+# Install nodejs
+echo "Installing nodejs..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 24
+node -v
+npm -v 
+echo "nodejs installed successfully."
+
+# Install claude code
+curl -fsSL https://claude.ai/install.sh | bash
+claude --version
